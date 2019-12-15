b1465ee7b493361491ca7cb6520c6fc2671f516e
Kiran Paturi <kpaturi@ipromoteu.com>
2019-12-15 15:57:58 +0530

    Fixed bracket close

--- node-backed/config/express.js
+++ node-backed/config/express.js
@@ -12,6 +12,7 @@ console.log(publicDir);
 app. use(function(req, res, next) {
     res. header("Access-Control-Allow-Origin", "*");
     res. header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept" );
+})
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
